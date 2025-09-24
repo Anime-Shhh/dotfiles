0f793afbb9945cb2cb602d8cad3d6a4b0bc57201
Animesh Jadhav <anime_shh@animeshs-mbp.lan>
2025-09-24 00:59:07 -0400

    added spicetify conf and more shortcuts

---
+++ raycast/.raycast/scripts/snippet-screenshot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# @raycast.schemaVersion 1
+# @raycast.title Snippet Screenshot
+# @raycast.mode silent
+# @raycast.packageName Custom
+
+# Trigger the built-in macOS screenshot tool for a selection
+screencapture -i ~/Downloads/Screenshots/snippet-$(date +%Y-%m-%d_%H-%M-%S).png
